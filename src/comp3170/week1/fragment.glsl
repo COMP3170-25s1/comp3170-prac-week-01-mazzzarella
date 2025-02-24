28dffdc9d0cabf9f29ca4e06784d2ed3a84f6f7f
mazzzarella <88129896+mazzzarella@users.noreply.github.com>
2025-02-24 18:48:02 +1100

    Creating diamond
    Manhattan Distance
    center: (0.5,0.5)
    (p.x-0.5) -> horizontal dist from center
    (p.y-0.5) -> vertical dist from center

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,7 +7,7 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+ 	float d = abs(p.x - 0.5) + abs(p.y - 0.5);   // Manhattan Distance
   
    if (d < 0.5) {
       o_colour = vec4(u_colour, 1);
